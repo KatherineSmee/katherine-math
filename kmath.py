c93cb11a0a1296a986f730a1649736d924564518
jemurrayPhD <141434544+jemurrayPhD@users.noreply.github.com>
2024-03-10 22:51:49 -0400

    Added kmath.py

---
+++ kmath.py
@@ -0,0 +1,12 @@
+def mult(x,y):
+    return x*y
+def add(x,y):
+    return x+y
+def divide(x,y):
+    if y==0:
+        raise Exception("Cannot divide by zero!")
+    else:
+        return x/y
+    
+
+        
